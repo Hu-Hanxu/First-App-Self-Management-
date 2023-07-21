362d45e219fc5db2bb4ec3e3c01d9755035e4ba9
huhanxu123456@gmail.com <huhanxu123456@gmail.com>
2023-07-21 14:52:24 +0900

    修正の一時保存

--- MyApp-Frontend/myapp/src/pages/main.jsx
+++ MyApp-Frontend/myapp/src/pages/main.jsx
@@ -1,45 +1,44 @@
 import React from 'react';
-import '../App.css';
 import '../styles/main.css';
-import { Card } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const MainPage = () => {
+
+const Main = () => {
+  const navigate = useNavigate();
+
   const handleStudyManagement = () => {
-    // 学習管理画面に飛ぶ
+    navigate('/study');
   };
 
   const handleScheduleManagement = () => {
-    // 日程管理画面に飛ぶ
+    navigate('/schedule');
   };
 
   const handleChoiceHelper = () => {
-    // 選択ヘルパー画面に飛ぶ
+    navigate('/choice');
   };
 
   return (
     <div className='area'>
       <h1 className='title'>自己管理システム</h1>
       <div className='card-area'>
-        <Card className='card'>
-          <img className='img' src="/MyApp-Frontend/myapp/src/images/lesson.png" alt='学習管理'/>
+        <div className='card' onClick={handleStudyManagement}>
+          <img className='img' src="/images/lesson.png" alt='学習管理'/>
           <p className='text'>学習管理</p>
           <div className='border-top'/>
-        </Card>
-        <Card className='card'>
-          <img className='img' src="/MyApp-Frontend/myapp/src/images/schedule.png" alt='日程管理'/>
+        </div>
+        <div className='card' onClick={handleScheduleManagement}>
+          <img className='img' src="/images/schedule.png" alt='日程管理'/>
           <p className='text'>日程管理</p>
-          <div className='border-top'/>
-        </Card>
-        <Card className='card'>
-          <img className='img' src="/MyApp-Frontend/myapp/src/images/choice.png" alt='選択ヘルパー'/>
+          <div className='border-top'onClick={handleChoiceHelper}/>
+        </div>
+        <div className='card'>
+          <img className='img' src="/images/choice.png" alt='選択ヘルパー'/>
           <p className='text'>選択ヘルパー</p>
           <div className='border-top'/>
-        </Card>
+        </div>
       </div>
     </div>
   );
-}
-
-const styles = {
 };
-export default MainPage;
+export default Main;
